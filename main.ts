6647581fd75b940cd94a35bf5735bcce3d9829b8
jasonyaocoder <jason.yao.coder@gmail.com>
2021-01-24 17:19:00 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let leds = 0
+let strip2 = 0
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    `)
+let strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    strip2 = randint(0, 64)
+    leds = neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+    strip.show()
+    strip.setPixelColor(strip2, leds)
+    basic.pause(5)
 })
